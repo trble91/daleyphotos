67f98e4082c145780fee84a63381226fe4102c02
trble91 <daleyaaron@gmail.com>
2024-06-05 11:15:57 -0400

    found a way to reference pages

--- src/app/Photography/page.jsx
+++ src/app/Photography/page.jsx
@@ -6,16 +6,16 @@ import Image from "next/image";
 import { useParams } from "next/navigation";
 
 export default function Photography() {
+  const photos = [
+    { src: "/bride.jpg", href: "/lifestyle" },
+    { src: "/groom-large.jpg", href: "/portraits" },
+    { src: "/Bailey-bw.png", href: "/weddings" },
+    { src: "/yankee-Large.jpeg", href: "/events" },
+  ];
 
-  const imageSources = ["/bride.jpg", "/groom-large.jpg"];
-  const imageSources2 = ["/Bailey-bw.png", "/yankee-Large.jpeg"];
-
-  // const { imgCard } = useParams();
-
-  
   return (
     <>
-    {/* <Navbar /> */}
+      {/* <Navbar /> */}
       <div className="flex sticky flex-row text-center sm:max-w-[300px]  md:flex-row md:text-left xl:flex-row max-w-[400px] xl:px-5 min-h-screen justify-center xl: space-y-4 mx-auto items-center snap-center">
         <h3 className="absolute top-6 uppercase tracking-[17px] text-gray-700 text-md sm:text-gray-700 sm:text-base md:text-gray-700 lg:text-gray-700 xl:text-gray-800">
           Portraits Weddings & Events
@@ -37,7 +37,7 @@ export default function Photography() {
             }}
             className="flex relative flex-col bottom-20 top-2 text-center mt-20 snap-center"
           >
-            {imageSources.map((src, index) => (
+            {photos.map((src, index) => (
               <Image
                 key={index}
                 src={src}
@@ -65,7 +65,7 @@ export default function Photography() {
             }}
             className="flex relative flex-col bottom-20 top-2 text-center mt-20 snap-center"
           >
-            {imageSources2.map((src, index) => (
+            {photos.map((src, index) => (
               <Image
                 key={index}
                 src={src}
