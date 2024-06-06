c7e60457f2615f03b044f8926bdc8cdbd0f1c061
trble91 <daleyaaron@gmail.com>
2024-06-05 20:02:15 -0400

    fix jsconfig

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function Bio() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className="flex relative ml-10 text-center md:text-left xl:flex-row xl:px-10 min-h-screen xl:space-y-0 snap-center">
         <h3 className="absolute top-24 items-center uppercase tracking-[20px] text-neutral-800 text-2xl sm:text-neutral-800">
           About
@@ -16,7 +16,25 @@ export default function Bio() {
         </p>
         <div className="flex absolute flex-row text-start text-neutral-800 text-lg top-56 xl:flex-row max-w-[500px] items-center snap-center">
           <p className="flex relative text-left text-m">
-            Embarking on a photography journey that captures the essence of weddings, special events, and creative design, I strive to tell unique stories through my lens, preserving cherished moments and crafting visually stunning memories.
+            Embarking on a photography journey that captures the essence of
+            weddings, special events, and creative design, I strive to tell
+            unique stories through my lens, preserving cherished moments and
+            crafting visually stunning memories. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc dictum lectus ut lectus malesuada,
+            at vulputate mauris finibus. Quisque dui arcu, eleifend eu ex at,
+            suscipit rutrum erat. Pellentesque turpis lorem, condimentum a
+            lectus quis, tempor consequat diam. Aenean dolor dolor, laoreet at
+            leo maximus, semper dictum sapien. Donec id est posuere elit
+            faucibus tristique nec ut justo. Donec eleifend lorem ex, id congue
+            sapien tempus sed. Donec nisl tellus, rutrum pellentesque commodo
+            sed, ultricies eget augue. Morbi gravida ante posuere neque laoreet,
+            pellentesque pulvinar lectus porta. Integer tempor, arcu vel
+            placerat eleifend, nulla metus facilisis risus, nec pharetra eros
+            ante sed enim. Maecenas varius erat eget ex ultrices, vel tempor
+            ante commodo. Suspendisse ut consectetur tortor. Phasellus et ornare
+            sapien, a congue lacus. In accumsan a nunc id fringilla. Orci varius
+            natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. 
           </p>
         </div>
       </div>
