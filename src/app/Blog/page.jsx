062f01c47bae2370f99810d715fdb53487297bf5
trble91 <daleyaaron@gmail.com>
2024-06-11 23:22:43 -0400

    fixes blog

--- src/app/Blog/page.jsx
+++ src/app/Blog/page.jsx
@@ -15,8 +15,8 @@ export default function Blog() {
       <div className="relative ml-4 flex max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
         <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border">
           <Image
-            src=""
-            alt="ui/ux review check"
+            src="/assets/yankee-Large.jpeg"
+            alt=""
             layout="responsive"
             width={700}
             height={475}
@@ -24,33 +24,33 @@ export default function Blog() {
         </div>
         <div className="p-6">
           <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-            UI/UX Review Check
+          Best F-stop
           </h4>
           <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
-            Because it&apos;s about motivating the doers. Because I&apos;m here to follow my dreams and inspire others.
+            It&apos;s all opionated! However, the sweet spot for many lenses are within tht 2.0-4.0 range.
           </p>
         </div>
         <div className="flex items-center justify-between p-6">
           <div className="flex items-center -space-x-3">
-            <Image 
-              alt="natali craig"
+            {/* <Image 
+              alt=""
               src=""
               className="relative inline-block h-9 w-9 rounded-full border-2 border-white object-cover object-center hover:z-10"
               layout="fixed"
               width={36}
               height={36}
-            />
-            <Image 
-              alt="Tania Andrew"
+            /> */}
+            {/* <Image 
+              alt=""
               src=""
               className="relative inline-block h-9 w-9 rounded-full border-2 border-white object-cover object-center hover:z-10"
               layout="fixed"
               width={36}
               height={36}
-            />
+            /> */}
           </div>
           <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
-            January 10
+            27 Jul 
           </p>
         </div>
       </div> 
@@ -60,8 +60,8 @@ export default function Blog() {
         <div className="relative flex max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
           <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border">
             <Image
-              src=""
-              alt="ui/ux review check"
+              src="/weddings/rings.jpg"
+              alt=""
               layout="responsive"
               width={700}
               height={475}
@@ -69,15 +69,15 @@ export default function Blog() {
           </div>
           <div className="p-6">
             <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-              UI/UX Review Check
+              My First Wedding
             </h4>
             <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
-              Because it&apos;s about motivating the doers. Because I&apos;m here to follow my dreams and inspire others.
+              The day I shot my first wedding, and the tools I had with me at the time.
             </p>
           </div>
           <div className="flex items-center justify-between p-6">
             <div className="flex items-center -space-x-3">
-              <Image 
+              {/* <Image 
                 alt="natali craig"
                 src=""
                 className="relative inline-block h-9 w-9 rounded-full border-2 border-white object-cover object-center hover:z-10"
@@ -92,10 +92,10 @@ export default function Blog() {
                 layout="fixed"
                 width={36}
                 height={36}
-              />
+              /> */}
             </div>
             <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
-              January 10
+              01 Oct 
             </p>
           </div>
         </div>  
