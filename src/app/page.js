ffdfa366cc6609bf6cc05c03a4814e8a39dd8b7f
trble91 <daleyaaron@gmail.com>
2024-06-11 23:37:45 -0400

    removes strictmode

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,9 @@ import Homepage from "./Homepage/page";
 export default function Home() {
   return (
     <>
-      <React.StrictMode>
         <main className="min-h-screen flex-col items-center">
           <Homepage />
         </main>
-      </React.StrictMode>
     </>
   );
 }
