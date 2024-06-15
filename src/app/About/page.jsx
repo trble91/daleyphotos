1854cd384d66bf8dd2a49cd6b3688482feddb5c7
trble91 <daleyaaron@gmail.com>
2024-06-15 18:54:04 -0400

    spacing fixed

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -6,13 +6,12 @@ import React from "react";
 import Logo from "../Components/Logo";
 
 export default function Bio() {
-  const about = 
-    `My photography journey is a vibrant tapestry woven from my eclectic upbringing and artistic aspirations. Despite struggling in school, I was encouraged to explore creativity. Raised as a first-generation American to West Indian parents, music shaped my early years—from Al Green to Tupac, each note fueled my passion.
-Transitioning from music to photography, I found a new canvas to express emotions and stories. Each click of the shutter became a brushstroke in my visual narrative—a journey from capturing intimate portraits to vibrant landscapes.
-Driven by a thirst for knowledge and a desire to push boundaries, photography became my avenue to blend creativity and storytelling. From earning recognition to seeing my vision come to life, every photo tells a story of courage and artistic evolution.
-Join me on this journey where creativity knows no bounds. Together, let's inspire and illuminate the world one click at a time.
+  const about = `My photography journey is a vibrant tapestry woven from my eclectic upbringing and artistic aspirations. Despite struggling in school, I was encouraged to explore creativity. Raised as a first-generation American to West Indian parents, music shaped my early years—from Al Green to Tupac, each note fueled my passion.
+    Transitioning from music to photography, I found a new canvas to express emotions and stories. Each click of the shutter became a brushstroke in my visual narrative—a journey from capturing intimate portraits to vibrant landscapes.
+    Driven by a thirst for knowledge and a desire to push boundaries, photography became my avenue to blend creativity and storytelling. From earning recognition to seeing my vision come to life, every photo tells a story of courage and artistic evolution.
+    Join me on this journey where creativity knows no bounds. Together, let's inspire and illuminate the world one click at a time.
 
-`
+    `;
 
   return (
     <>
@@ -22,10 +21,9 @@ Join me on this journey where creativity knows no bounds. Together, let's inspir
           About
         </h3>
         <Logo />
-
         <div className="flex absolute flex-row text-start text-stone-200 sm:text-center text-lg top-56 xl:flex-row max-w-[700px] items-center snap-center">
           <p className="flex relative ml-4 text-left font-thin text-m mb-8">
-           {about}
+            {about}
           </p>
         </div>
       </div>
